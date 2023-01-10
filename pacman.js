c13fa8c3d0d19adde8da693bb124b0b97271c16f
Ahmad Karimi <zikman@gmail.com>
2023-01-09 20:09:22 -0500

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,10 +1,10 @@
 var pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['images/PacMan1.png', 'images/PacMan2.png'],
+  ['images/PacMan3.png', 'images/PacMan4.png'],
 ];
 var direction = 0;
-const pacMen = [];
+const pacMen = []; // This array holds all the pacmen
 
 function setToRandom(scale) {
   return {
@@ -12,21 +12,23 @@ function setToRandom(scale) {
     y: Math.random() * scale,
   };
 }
-// Factory to make a PacMan
+// Factory to make a PacMan at a random position with random velocity
 function makePac() {
-  // returns an object with values scaled {x: 33, y: 21}
-  let velocity = setToRandom(10);
+  // returns an object with random values scaled {x: 33, y: 21}
+  let velocity = setToRandom(10); // {x:?, y:?}
   let position = setToRandom(200);
   // Add image to div id = game
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
+  newimg.src = 'images/PacMan1.png';
   newimg.width = 100;
   newimg.style.left = position.x;
   newimg.style.top = position.y;
+
+  // add new Child image to game
   game.appendChild(newimg);
-  // new style of creating an object
+  // return details in an object
   return {
     position,
     velocity,
@@ -34,7 +36,11 @@ function makePac() {
   };
 }
 
+let counter = 0;
 function update() {
+  //counter to open and close Pacmans mouth
+  counter++;
+  let mouthOpen = Math.floor(counter / 10) % 2;
   //loop over pacmen array and move each one and move image in DOM
   pacMen.forEach((item) => {
     checkCollisions(item);
@@ -43,6 +49,12 @@ function update() {
 
     item.newimg.style.left = item.position.x;
     item.newimg.style.top = item.position.y;
+
+    if (item.velocity.x > 0) {
+      item.newimg.src = pacArray[0][mouthOpen];
+    } else {
+      item.newimg.src = pacArray[1][mouthOpen];
+    }
   });
   setTimeout(update, 20);
 }
@@ -62,9 +74,4 @@ function checkCollisions(item) {
 
 function makeOne() {
   pacMen.push(makePac()); // add a new PacMan
-}
-
-//don't change this line
-if (typeof module !== 'undefined') {
-  module.exports = { checkCollisions, update, pacMen };
 }
